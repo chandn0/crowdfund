d36b6ead98e48e8b09a641acf6ae28a6693bd251
chandn0 <boinapallychandan007@gmail.com>
2023-02-08 14:36:01 +0530

    first commit

---
+++ contracts/Lock.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Lock {
+    uint public unlockTime;
+    address payable public owner;
+
+    event Withdrawal(uint amount, uint when);
+}
---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    buildbear: {
+      url: "https://rpc.dev.buildbear.io/Solid_Wedge_Antilles_fdcf2516",
+      accounts: ["5849c78835ad2825b86fd1537bfd6ff9414a40f45598760234e42e05eba1c654"],
+      // url: "https://rpc.dev.buildbear.io/Naughty_Ratts_Tyerel_abcc8bba",
+    },
+  },
+  etherscan: {
+    apiKey: {
+      buildbear: "test1",
+    },
+    customChains: [
+      {
+        network: "buildbear",
+        chainId: 8350,
+        urls: {
+          apiURL:
+            "https://rpc.dev.buildbear.io/Solid_Wedge_Antilles_fdcf2516",
+          browserURL: "https://explorer.dev.buildbear.io/Solid_Wedge_Antilles_fdcf2516",
+        },
+      },
+    ],
+  },
+};
